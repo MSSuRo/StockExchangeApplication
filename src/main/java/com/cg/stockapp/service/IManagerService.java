09f1d40be46088ee76e202f5dea6829d107a1121
MSSuRo <70026590+MSSuRo@users.noreply.github.com>
2021-04-11 15:49:26 +0530

    Update ManagerService

--- src/main/java/com/cg/stockapp/service/IManagerService.java
+++ src/main/java/com/cg/stockapp/service/IManagerService.java
@@ -1,9 +1,19 @@
 package com.cg.stockapp.service;
 
+import java.util.List;
+
 import com.cg.stockapp.dto.Manager;
 
-public interface IManagerService {
-
-	public boolean addManager(Manager manager);
+public interface ManagerService {
 	
+	public boolean addManager(Manager man);
+	
+	
+	public List<ManagerService> getAllManager(int ManagerId);
+	
+	
+	public Manager updateManager(Manager info);
+	public Manager deleteManager(Manager man);
+
+	//public int getManagerDetails(int ManagerId);
 }
