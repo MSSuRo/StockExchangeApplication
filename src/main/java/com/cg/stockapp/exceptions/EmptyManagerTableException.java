f70b77c3676a6cdffe1dea69e1dae23223afb9b0
MSSuRo <70026590+MSSuRo@users.noreply.github.com>
2021-04-11 15:52:11 +0530

    added two exceptions

---
+++ src/main/java/com/cg/stockapp/exceptions/EmptyManagerTableException.java
@@ -0,0 +1,14 @@
+package com.cg.stockapp.exception;
+
+
+
+public class EmptyManagerTableException extends RuntimeException {
+	
+	public EmptyManagerTableException(String message) {
+		super(message);
+	}
+	
+	
+	
+	}
+
