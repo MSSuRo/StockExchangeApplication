470f90c5d97d5fe89152fc105261914c9c634dde
MSSuRo <70026590+MSSuRo@users.noreply.github.com>
2021-04-12 08:44:02 +0530

    Update ManagerController.java

--- src/main/java/com/cg/stockapp/controllers/ManagerController.java
+++ src/main/java/com/cg/stockapp/controllers/ManagerController.java
@@ -1,26 +1,61 @@
-package com.cg.stockapp.controllers;
+package com.cg.stockapp.controller;
+
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.stockapp.dto.Manager;
-import com.cg.stockapp.service.ManagerService;
+import com.cg.stockapp.entities.Manager;
+import com.cg.stockapp.service.ManagerSerive;
 
 @RestController
 @RequestMapping("/manager")
 public class ManagerController {
 
 	@Autowired
-	private ManagerService serv;
-
-	@PostMapping
-	public String addManager(@RequestBody Manager manager) {
-		serv.addManager(manager);
+	private ManagerService managerService;
+	
+	@PostMapping("/manager")
+	public String addManager(@RequestBody Manager man) {
+		managerService.addManager(man);
 		return "Manager added successfully";
-
+		
 	}
+	
+	@GetMapping
+	public List<Manager> getAllManager() {
 
+		return managerService.getAllManager();
+		
+	}
+	
+	@UpdateMapping
+	public String updateManager(@RequestBody Manager man) {
+		managerService.updateManager(man);
+		return "Manager updated successfully";
+	
+	}
+	
+	
+	
+	
+	@DeleteMapping
+	public String deleteManager(@RequestBody Manager man) {
+		managerService.deleteManager(man);
+		return "Manager with id "+man.getManagerId()+" successfully deleted";
+	}
+	
+	
 }
+
+
+
+
+
+
